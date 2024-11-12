093e38c89be1a5515763b07f9262af1e3227f41f
idaspa <idahelenhal@msn.com>
2024-11-12 10:01:49 +0100

    first

---
+++ dictionary.mjs
@@ -0,0 +1,18 @@
+const dictionary = {
+no: { 
+    wrongGuesses: "Feil gjetninger: ",
+    guessPrompt: "Gjett en bokstav eller ord: ",
+    winCelibration: "Hurra du gjettet ordet"
+},
+
+en: {
+    wrongGuess: "You guessed wrong: ",
+    guessPrompt: "Guss a letter or word: ",
+    winCelibration: "my, my, my, you guessed the word"
+},
+
+};
+
+
+
+export default dictionary
